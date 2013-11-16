8a7ca45626096fb213e4baaff98425d0e5364778
mimouncadosch <mimouncadosch@gmail.com>
2013-11-16 12:30:07 -0500

    twitter authorization

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ var express = require('express'),
   routes = require('./routes'),
   api = require('./routes/api'),
   http = require('http'),
-  path = require('path');
+  path = require('path'),
+  passport = require('passport'),
+  TwitterStrategy = require('passport-twitter').Strategy;
+
 
 var app = module.exports = express();
 
@@ -23,6 +26,8 @@ app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({secret:'nytimes'}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
@@ -35,6 +40,22 @@ if (app.get('env') === 'development') {
 if (app.get('env') === 'production') {
   // TODO
 };
+
+/**
+* Twitter authorization configuration
+*/
+
+passport.use(new TwitterStrategy({
+    consumerKey: "rKxEEYe0OXdNC2Wh2qlXDA",
+    consumerSecret: "JMfgdIUkUJj7Mfsdk1xAzD0M2dQ4dHkSjsXdWFtWVw",
+    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
 
 
 /**
@@ -50,6 +71,17 @@ app.get('/members', api.get_member_info);
 
 // redirect all others to the index (HTML5 history)
 // app.get('*', routes.index);
+
+// Twitter Routes
+app.get('/auth/twitter',
+  passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback', 
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 /**
  * Start Server
