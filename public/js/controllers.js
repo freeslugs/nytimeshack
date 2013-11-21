bc4b9479ff05dbf1d10149c0cf082559bc7dbac4
Gilad Penn <freeslugs@gmail.com>
2013-11-21 18:19:20 -0500

    backt o backend

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,41 +3,48 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('AppCtrl', function ($scope, $http, $rootScope) {
-    $http({
-      method: 'GET',
-      url: '/members'
-    }).
-    success(function (data, status, headers, config) {
-      $rootScope.people = data;
-      console.log($scope.people);
-    }).
-    error(function (data, status, headers, config) {
-      $rootScope.people = 'Error!'
-    });
+  controller('AppCtrl', function ($scope, $http) {
+    
   }).
   controller('MyCtrl1', function ($scope, $http) {
     //while(!$scope.people) {$scope.loading = true;}
+    
     $scope.states = ['', 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'];
 
     $scope.message = '';
 
-    $scope.selectedPeople = [];
+    $scope.change = function() { 
+      $http({
+        method: 'GET',
+        url: '/members',
+        params: $scope.search
+      }).
+      success(function (data, status, headers, config) {
+        $scope.people = data;
+        console.log($scope.people);
+      }).
+      error(function (data, status, headers, config) {
+        $scope.people = 'Error!'
+      });
+    };
+
+    /*$scope.selectedPeople = [];
     $scope.selectPerson = function(index) {
       $scope.selectedPeople.push($scope.people[index]);
     }
-    //this is the function to post tweet
+    //this is the function to post tweet*/
     $scope.posttweet = function(message) {
+      console.log($scope.people);
       $scope.twitterhandles = "";
-      if($scope.selectedPeople.length == 0) {
+      /*if($scope.selectedPeople.length == 0) {
         $scope.selectedPeople = $scope.people;
-      }
-      for (var i = $scope.selectedPeople.length - 1; i >= 0; i--) {
+      }*/
+      /*for (var i = $scope.selectedPeople.length - 1; i >= 0; i--) {
         console.log($scope.selectedPeople);
         if($scope.selectedPeople[i].twitter_id) {
           $scope.twitterhandles += $scope.selectedPeople[i].twitter_id + ",";
         }
-      };
+      };*/
       $http({
         method: 'POST',
         //headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
